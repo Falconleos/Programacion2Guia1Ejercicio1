a5b553ba3b650c0e43d562fbef5139e3a328f5d5
Usuario <leonelfsotopublic@gmail.com>
2024-08-18 20:54:11 -0300

    creacion clase empleados y salario anual

--- src/Model/Empleados.java
+++ src/Model/Empleados.java
@@ -1,13 +1,64 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Model;
 
-/**
- *
- * @author Usuario
- */
+
 public class Empleados {
+    
+    //Class's Attributes
+    String dni;
+    String name;
+    String surname;
+    Float salary;
+    
+    //Getters & Setters
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+    
+    public void setDni(String dni){
+        this.dni = dni;
+    }
+    
+    public String getDni(){
+        return this.dni;
+    }
+    
+    public void setSurname(String surname){
+        this.surname = surname;
+    }
+    
+    public String getSurname(){
+        return this.surname;
+    }
+    
+    public void setSalary(Float salary){
+        this.salary = salary;
+    }
+    
+    public Float getSalary(){
+        return this.salary;
+    }
+    
+    //Constructor
+    public Empleados(String dni, String name, String surname, Float salary){
+        this.dni = dni;
+        this.name = name;
+        this.surname = surname;
+        this.salary = salary;
+    }
+    
+    //Methods
+    
+        //calculate anual salary
+        public Float getAnnualSalary(Float salary){
+            return this.salary * 12;
+        }
+        
+        
+    
     
 }
--- src/Services/Programacion2Guia1Ejercicio1.java
+++ src/Services/Programacion2Guia1Ejercicio1.java
@@ -1,20 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
+
 package Services;
 
-/**
- *
- * @author Usuario
- */
+    import Model.Empleados;
+
 public class Programacion2Guia1Ejercicio1 {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Empleados employee1 = new Empleados("34946165","Leonel","Soto",1800f);
+        
     }
     
 }
